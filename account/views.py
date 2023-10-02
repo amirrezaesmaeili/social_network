3a8d1539c39455865c093e12acd9c83b3f28d79e
amirrezaesmaeili <esmaili69315@gmail.com>
2023-10-01 23:12:45 -0400

    pref : added varieble name to class registeration

--- account/views.py
+++ account/views.py
@@ -5,14 +5,18 @@ from django.contrib.auth.models import User
 from django.contrib import messages
 
 class RegisterationView(View):
+    form_class = RegisterationForm
+    template_name = 'account/register.html'
+
     def get(self,request):
-        form = RegisterationForm()
-        return render(request,'account/register.html',{'form':form})
+        form = self.form_class
+        return render(request,self.template_name,{'form':form})
     
     def post(self,request):
-        form = RegisterationForm(request.POST)
+        form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
             User.objects.create_user(cd["user_name"] , cd["email"] , cd["password"])
             messages.success(request , "your registerd successfuly" , "success")
             return redirect("home:home")
+        return render(request,self.template_name,{"form":form})
