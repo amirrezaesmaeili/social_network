14ca8deee8091195bca488ee9a99d9aecc53d076
amirrezaesmaeili <esmaili69315@gmail.com>
2023-10-01 23:01:30 -0400

    feat : added create user method to Register

--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views import View
 from .forms import RegisterationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 class RegisterationView(View):
     def get(self,request):
@@ -8,4 +10,9 @@ class RegisterationView(View):
         return render(request,'account/register.html',{'form':form})
     
     def post(self,request):
-        pass
+        form = RegisterationForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd["user_name"] , cd["email"] , cd["password"])
+            messages.success(request , "your registerd successfuly" , "success")
+            return redirect("home:home")
