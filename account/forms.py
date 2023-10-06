3af34938369bbe2b2f5f5132c1bb9cc82e50d2ea
amirrezaesmaeili <esmaili69315@gmail.com>
2023-10-06 02:01:50 -0400

    feat : create & implement login page

--- account/forms.py
+++ account/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 class RegisterationForm(forms.Form):
-    user_name = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     password1 = forms.CharField(label="password",widget=forms.PasswordInput(attrs={'class':'form-control' }))
     password2 = forms.CharField(label="confrim password",widget=forms.PasswordInput(attrs={'class':'form-control' }))
@@ -16,12 +16,12 @@ class RegisterationForm(forms.Form):
             raise ValidationError(" this email has been exists ")
         return email
     
-    def clean_user_name(self):
-        user_name = self.cleaned_data['user_name']
-        user = User.objects.filter(username=user_name).exists()
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
         if user :
             raise ValidationError("this username has been exists")
-        return user_name
+        return username
     
     def clean(self):
         cd = super().clean()
@@ -29,3 +29,8 @@ class RegisterationForm(forms.Form):
         p2 = cd.get("password2")
         if p1 and p2 and p1 != p2:
             raise ValidationError("password must match") 
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
--- account/views.py
+++ account/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render,redirect
 from django.views import View
-from .forms import RegisterationForm
+from .forms import RegisterationForm,LoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import login,authenticate
 
-class RegisterationView(View):
+class UserRegisterView(View):
     form_class = RegisterationForm
     template_name = 'account/register.html'
 
@@ -16,7 +17,35 @@ class RegisterationView(View):
         form = self.form_class(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            User.objects.create_user(cd["user_name"] , cd["email"] , cd["password1"])
+            User.objects.create_user(cd["username"] , cd["email"] , cd["password1"])
             messages.success(request , "your registerd successfuly" , "success")
             return redirect("home:home")
         return render(request,self.template_name,{"form":form})
+
+
+class UserLoginView(View):
+    form_class = LoginForm
+    template_name = "account/login.html"
+
+
+    def get(self,request):
+        form = self.form_class 
+        return render(request , self.template_name , {'form':form} )
+
+    def post(self,request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate( request , username=cd["username"], password=cd["password"] )
+            if user is not None:
+                login(request,user)
+                messages.success(request , "you login successfuly" , "success")
+                return redirect("home:home")
+            messages.error(request , "password or username not correct" 'warning')
+        return render(request ,self.form_class,{'form':form})
+    
+    
+
+
+
+        
