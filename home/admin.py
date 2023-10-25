df3cfbd1fab5e7106593f540eac6608f952d571e
amirrezaesmaeili <esmaili69315@gmail.com>
2023-10-24 22:47:24 -0400

    feat: implemented admin panel & customize

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display =("user","slug","created")
+    search_fields = ("slug",)
+    list_filter = ("updated",)
+    raw_id_fields = ("user",)
+    prepopulated_fields = {"slug":("body",)}
